4f1e69b70192d7fab3ee447fc67bbab5e9a1a40b
Farigorn <Excalibur12@yandex.ru>
2021-03-03 21:07:00 +0500

    Класс Util теперь Singleton. Ресурсы в Dao теперь закрываются

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -11,8 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private Statement statement;
-    private Util util = new Util();
+    private Util util = Util.getUtil();
 
     public UserDaoJDBCImpl() {
 
@@ -20,8 +19,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
 
     public void createUsersTable() {
-        try {
-            statement = util.getConnection().createStatement();
+        try (Statement statement = util.getConnection().createStatement()) {
             statement.execute("CREATE TABLE IF NOT EXISTS users (Id INTEGER not null PRIMARY KEY AUTO_INCREMENT, " +
                     "Name VARCHAR(45), " +
                     "Lastname VARCHAR(45), " +
@@ -32,8 +30,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        try {
-            statement = util.getConnection().createStatement();
+        try (Statement statement = util.getConnection().createStatement()) {
             statement.execute("DROP TABLE  IF EXISTS users;");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -41,21 +38,19 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            PreparedStatement preparedStatement = util.getConnection().prepareStatement("INSERT INTO users (name, lastname, age) VALUES (?, ?, ?)");
+        try (PreparedStatement preparedStatement = util.getConnection().prepareStatement("INSERT INTO users (name, lastname, age) VALUES (?, ?, ?)")) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setInt(3, age);
             preparedStatement.executeUpdate();
-            System.out.println("User с именем – " + name + " добавлен в базу данных" );
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
     public void removeUserById(long id) {
-        try {
-            PreparedStatement preparedStatement = util.getConnection().prepareStatement("DELETE FROM users WHERE id = ?");
+        try (PreparedStatement preparedStatement = util.getConnection().prepareStatement("DELETE FROM users WHERE id = ?")) {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
         } catch (SQLException throwables) {
@@ -65,8 +60,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
-        try {
-            statement = util.getConnection().createStatement();
+        try (Statement statement = util.getConnection().createStatement()) {
             ResultSet rs = statement.executeQuery("SELECT * from users");
             while (rs.next()) {
                 String name = rs.getString("name");
@@ -81,8 +75,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try {
-            statement = util.getConnection().createStatement();
+        try (Statement statement = util.getConnection().createStatement()) {
             statement.execute("TRUNCATE TABLE users;");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -10,13 +10,22 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/users_base?useSSL=false";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
+    private static Util util;
     private Connection connection;
 
-    public Util() {
+    private Util() {
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        }
+    }
+
+    public static Util getUtil() {
+        if (util == null) {
+            return util = new Util();
+        } else {
+            return util;
         }
     }
 
